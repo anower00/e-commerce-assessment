ef68c03653833cfe27c1e7db46ccd2c24b7aa938
anower00 <anower.hasan@gmail.com>
2019-05-04 22:21:44 +0600

    registration with supplier and company and redirect their pages after registration and login

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function productSupplier()
+    {
+        return view('supplier.index');
+    }
+
+    public function company()
+    {
+        return view('company.index');
+    }
+}
---
+++ resources/views/company.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+
+<h1>This is company page</h1>
+
+</body>
+</html>
